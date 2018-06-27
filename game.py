edbbaecab4b86b421cf8588c54686e536cf81251
sherryyohoo <scai@smith.edu>
2018-06-27 12:09:25 -0400

    Added Nutrition class

--- game.py
+++ game.py
@@ -8,20 +8,58 @@ import cocos
 from cocos.director import director
 from cocos.actions import *
 
+#class for nutrition
+class NutritionBar(cocos.layer.Layer):
+    
+    def __init__(self):
+        super(NutritionBar,self).__init__()
+        
+        #Draw nutritionbar
+        self.nutritionbar=cocos.sprite.Sprite('NutritionBar.png')
+        self.nutritionbar.scale_y=0.2
+        self.nutritionbar.scale_x=0.2        
+        self.nutritionbar.position=320-self.nutritionbar.width/2,260
+        self.nutritionbar.image_anchor=0,0
+        self.add(self.nutritionbar)
+
+        #Draw nutritionicon
+        self.nutritionicon=cocos.sprite.Sprite('NutritionIcon.png')
+        self.nutritionicon.scale_y=0.0625
+        self.nutritionicon.scale_x=0.0625
+        self.nutritionicon_initial=300-self.nutritionbar.width/2
+        self.nutritionicon.position=self.nutritionicon_initial,275
+        self.nutritionicon.image_anchor=0,0
+        self.add(self.nutritionicon)
+        
+    # get value of nutritionicon
+    def get_value(self):
+        position=self.nutritionicon.x-self.nutritionicon_initial
+        return(position)
+
+    def set_value(self,speed):
+        #move=MoveBy((,0))
+        #self.watericon.do(move)
+        if(self.get_value()<=self.nutritionbar.width):
+            print(self.nutritionicon.x)
+            self.nutritionicon.x+=speed
+        else:
+            self.reset()
+
+    def reset(self):
+        self.speed=0
+        self.nutritionicon.position=self.nutritionicon_initial,315
+        
 #class for water
 class WaterBar(cocos.layer.Layer):
     
     def __init__(self):
         super(WaterBar,self).__init__()
-        self.speed=0
         
         #Draw waterbar
         self.waterbar=cocos.sprite.Sprite('WaterBar.png')
         self.waterbar.scale_y=0.2
         self.waterbar.scale_x=0.2        
-        rect_Bar=self.waterbar.get_rect()
-        bar_X=rect_Bar.get_right()-rect_Bar.get_left()
-        self.waterbar.position=320-bar_X/2,300
+        self.waterbar.position=320-self.waterbar.width/2,300
         self.waterbar.image_anchor=0,0
         self.add(self.waterbar)
 
@@ -29,10 +67,9 @@ class WaterBar(cocos.layer.Layer):
         self.watericon=cocos.sprite.Sprite('WaterIcon.png')
         self.watericon.scale_y=0.02
         self.watericon.scale_x=0.02
-        self.watericon_initial=300-bar_X/2
+        self.watericon_initial=300-self.waterbar.width/2
         self.watericon.position=self.watericon_initial,315
         self.watericon.image_anchor=0,0
-        self.set_value(self.speed)
         self.add(self.watericon)
         
     # get value of watericon
@@ -94,6 +131,10 @@ class InputVoice(cocos.layer.Layer):
         #get water
         self.water=WaterBar()
         self.add(self.water)
+
+        #add nutrition
+        self.nutrition=NutritionBar()
+        self.add(self.nutrition)
         self.schedule(self.update)
         
     def on_mouse_press(self, x, y, buttons, modifiers):
@@ -107,13 +148,10 @@ class InputVoice(cocos.layer.Layer):
         volume=np.sum(sample**2)/len(sample)
         if(volume > 0.0002):
             self.water.set_value(1)
-            self.water.speed= self.water.speed+1
-            print("Value")
-            print(self.water.speed)
+            self.nutrition.set_value(2)
+            print(self.water.get_value())
+            print(self.water.get_value())
             
-            self.water.add(self.water.watericon)
- #           self.water.remove(self.water.watericon)
- #           self.water.add(self.water.watericon)
             
         volume="{:.6f}".format(volume)
         #print(dt)
