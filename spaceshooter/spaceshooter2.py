cfb6cac1433ee08d1ac530218ca66c878a311f5a
xriswhat <xtan@smith.edu>
2018-07-06 14:12:18 -0400

    Update spaceshooter2

--- spaceshooter/spaceshooter2.py
+++ spaceshooter/spaceshooter2.py
@@ -23,11 +23,120 @@ pygame.init()
 window = pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
 
+#Draw stars
+def add_star():
+    star = Sprite()
+    star_size = random.randrange(1,4)
+    star.x = random.randrange(0, window.get_width())
+    star.y = 0
+    star.image = pygame.Surface((star_size,star_size))
+    star.image.fill((255,255,255))
+    stars.append(star)
+
+#blinking images
+sparks = ["assets2/spark_1.png","assets2/spark_2.png","assets2/spark_3.png","assets2/spark_4.png",
+         "assets2/spark_5.png","assets2/spark_6.png","assets2/spark_7.png","assets2/spark_8.png",
+         "assets2/spark_9.png","assets2/spark_8.png","assets2/spark_7.png","assets2/spark_6.png",
+         "assets2/spark_5.png","assets2/spark_4.png","assets2/spark_3.png","assets2/spark_2.png",
+         "assets2/spark_1.png"]
+
+## global list for testing volume and pitch
+volume_avg_list = []
+volume_avg = 0.0003
+volume_std=0.0002
+
+
 #Initialize menu
 def main_menu():
     global screen
     menu_song = pygame.mixer.music.load("sounds/menu.mp3")
+    pygame.mixer.music.play(-1)
+    title = pygame.image.load("assets2/Logan_space_title.png")
+    count = 0
+    spark = sparks[0]
+    pygame.display.flip()
+
+    while True:
+        count = count + 1
+        if count%19 == 0:
+            spark = sparks[0]
+        elif count%19 == 1:
+            spark = sparks[1]
+        elif count%19 == 2:
+            spark = sparks[2]
+        elif count%19 == 3:
+            spark = sparks[3]
+        elif count%19 == 4:
+            spark = sparks[4]
+        elif count%19 == 5:
+            spark = sparks[5]
+        elif count%19 == 6:
+            spark = sparks[6]
+        elif count%19 == 7:
+            spark = sparks[7]
+        elif count%19 == 8:
+            spark = sparks[8]
+        elif count%19 == 9:
+            spark = sparks[9]
+        elif count%19 == 10:
+            spark = sparks[9]
+        elif count%19 == 11:
+            spark = sparks[9]
+        elif count%19 == 12:
+            spark = sparks[8]
+        elif count%19 == 13:
+            spark = sparks[7]
+        elif count%19 == 14:
+            spark = sparks[6]
+        elif count%19 == 15:
+            spark = sparks[5]
+        elif count%19 == 16:
+            spark = sparks[4]
+        elif count%19 == 17:
+            spark = sparks[3]
+        elif count%19 == 18:
+            spark = sparks[2]
+        elif count%19 == 19:
+            spark = sparks[1]
+        
+        ev = pygame.event.poll()
+        if ev.type == pygame.KEYDOWN:
+            if ev.key == pygame.K_RETURN:
+                break
+            elif ev.key == pygame.K_q:
+                pygame.quit()
+                quit()
+        elif ev.type == pygame.QUIT:
+                pygame.quit()
+                quit() 
+        else:
+            enter = pygame.image.load("assets2/subtitle_1.png")
+            quitt = pygame.image.load("assets2/subtitle_2.png")
+            spark_draw = pygame.image.load(spark)
+            window.blit(spark_draw,(40,70))
+            window.blit(title,(120,100))
+            window.blit(enter,(120,520))
+            window.blit(quitt,(120,580))
+            pygame.display.flip()
+            clock.tick(7)
+            window.fill(pygame.Color("black"))
+            
+def voice_test():
+    test_1 = pygame.image.load("assets2/test_1.png")
+    window.blit(test_1,(0,0))
+    pygame.display.flip()
     
+    
+    
+main_menu()
+pygame.time.wait(3000)
+pygame.mixer.music.stop()
+
+voice_test()
+pygame.time.wait(10000)
+
+
+#####################################################################################################
 class Sprite:
     pass
 
@@ -54,14 +163,7 @@ def add_alien():
 #        aliens.remove(alien)
     aliens.append(alien)
     
-def add_star():
-    star = Sprite()
-    star_size = random.randrange(1,4)
-    star.x = random.randrange(0, window.get_width())
-    star.y = 0
-    star.image = pygame.Surface((star_size,star_size))
-    star.image.fill((255,255,255))
-    stars.append(star)
+
 
 def get_sprite_rectangle(sprite):
     return sprite.image.get_rect().move(sprite.x, sprite.y)
