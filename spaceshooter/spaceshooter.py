8bf8751606856e92762a31b8291a9e1285f1c274
hz97 <hzheng@smith.edu>
2018-07-06 13:12:47 -0400

    Stop the Space Ship Going Out the Window; Making The Bullets Destroy Aliens; Destroy the ship when lives run out; Restart the game when space is pressed; Keep track of a high score

--- spaceshooter/spaceshooter.py
+++ spaceshooter/spaceshooter.py
@@ -1,98 +1,270 @@
-#package for audio
+import pygame
+import sys
+import random
 import pyaudio
 import aubio
 import numpy as np
-import random
 
-#package for game
-import cocos
-from cocos.director import director
-from cocos.actions import *
+#Audio initialization
+CHUNK = 1024
+RATE = 44100
+voice = pyaudio.PyAudio()
+stream = voice.open(format=pyaudio.paFloat32, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNK)
+voiceDetection = aubio.pitch("default", CHUNK*2, CHUNK, RATE)
+voiceDetection.set_unit("Hz")
+voiceDetection.set_silence(-40)
 
-from cocos.layer import ScrollingManager, ScrollableLayer, ColorLayer
-from cocos.tiles import load
-from cocos.tiles import MapLayer
+#Constants definition
+WIDTH = 540
+HEIGHT = 720
 
-from pyglet.window.key import symbol_string
+#Initialize the game
+pygame.init()
+window = pygame.display.set_mode((WIDTH,HEIGHT))
+clock = pygame.time.Clock()
+
+#Initialize menu
+def main_menu():
+    global screen
+    menu_song = pygame.mixer.music.load("sounds/menu.mp3")
+
+class Sprite:
+    pass
+
+def display_sprite(sprite):
+    window.blit(sprite.image, (sprite.x, sprite.y))
+
+def fire_bullet():
+    bullet = Sprite()
+    bullet.x = ship.x + 20
+    bullet.y = ship.y - 10
+    bullet.image = bullet_image
+    bullet.used = False
+    bullets.append(bullet)
+
+def add_alien():
+    alien = Sprite()
+    alien.x = random.randrange(0, window.get_width()-alien_image.get_width())
+    alien.y = 10
+    alien.image = alien_image
+    alien.hit = False
+    alien.alpha = 255
+    aliens.append(alien)
+
+def add_star():
+    star = Sprite()
+    star_size = random.randrange(1,4)
+    star.x = random.randrange(0, window.get_width())
+    star.y = 10
+    star.image = pygame.Surface((star_size,star_size))
+    star.image.fill((255,255,255))
+    stars.append(star)
+
+def get_sprite_rectangle(sprite):
+    return sprite.image.get_rect().move(sprite.x, sprite.y)
+
+#Creating score bar
+font = pygame.font.Font(None,24)
+foreground = (200,200,200)
+#Creating the background
+background = (0,0,0)
+#Creating the spaceship
+ship_image = pygame.image.load("assets2/spaceship_0.png")
+#ship_image = pygame.transform.scale(ship_image, (70, 70))
+#Creating the bullet
+bullet_image = pygame.image.load("assets2/bullet_0.png")
+#bullet_image = pygame.transform.scale(bullet_image, (20, 40))
+#Creating aliens
+alien_image = pygame.image.load("assets2/alien_0.png")
+#alien_image = pygame.transform.scale(alien_image, (70, 70))
+aliens = []
+frames_until_next_alien = random.randrange(30, 100)
+#Creating stars
+stars = []
+frames_until_next_star = 0
 
 
-global WIDTH, HEIGHT
-WIDTH=375
-HEIGHT=660
-class Bullet(cocos.layer.Layer):
 
-	def __init__(self, x, y):
-		super(Bullet, self).__init__()
-		self.bullet=cocos.sprite.Sprite('assets2/bullet.png')
-		self.bullet.scale_y=0.03
-		self.bullet.scale_x=0.03
-		self.bullet.image_anchor=0,0
-		self.bullet.position=x+(62.5-self.bullet.width/2), y+50
-		self.bullet.do(MoveBy((0,600),3))
-		self.add(self.bullet)
+#Initializing ship's coordinates
+ship = Sprite()
+ship.x = window.get_width() / 2
+ship.y = window.get_height() - 10
+ship.red = 0
+ship.alpha = 0
+ship.image = ship_image
 
+#Initializing Score Bar
+score = 0
+highest_score = 0
+lives = 3
 
-
-
-class Spaceship(cocos.layer.Layer):
-	is_event_handler=True
-
-	def __init__(self):
-		super(Spaceship,self).__init__()
-		self.CHUNK=1024
-		self.RATE=44100
-
-        #init voice input
-		p=pyaudio.PyAudio()
-
-        #Open stream
-		self.stream = p.open(format=pyaudio.paFloat32, channels=1, rate=self.RATE, input=True, frames_per_buffer=self.CHUNK)
-        # Aubio's pitch detection
-		self.pDetection = aubio.pitch("default", self.CHUNK*2, self.CHUNK, self.RATE)
-
-        #Set unit.
-		self.pDetection.set_unit("Hz")
-		self.pDetection.set_silence(-40)
-
-		self.bullets=cocos.cocosnode.CocosNode()
-		self.add(self.bullets)
-
-        #Draw spaceship
-		self.spaceship=cocos.sprite.Sprite('assets2/sapceship.png')
-		self.spaceship.scale_y=0.25
-		self.spaceship.scale_x=0.25
-		self.spaceship.image_anchor=0,0
-		self.spaceship.position=375/2-self.spaceship.width/2, 30
-		self.add(self.spaceship)
+bullets = []
 
 
-		self.schedule(self.update)
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEMOTION:
+            print ("mouse at (%d, %d)" % event.pos)
+        elif event.type == pygame.QUIT:
+            sys.exit()
+        elif (event.type == pygame.KEYDOWN) and (event.key == pygame.K_SPACE) and (lives == 0) and (ship.alpha == 0):
+            ship = Sprite()
+            ship.x = window.get_width() / 2
+            ship.y = window.get_height() - 10
+            ship.red = 0
+            ship.alpha = 0
+            ship.image = ship_image
 
-	def on_key_press(self, key, modifiers):
-		move_left=MoveBy((-20,0),0.5)
+            score = 0
+            lives = 3
+            bullets = []
+            aliens = []
+            stars = []
+            frames_until_next_alien = 50
+            frames_until_next_star = 0
 
-		if self.spaceship.x>=0+20-self.spaceship.width/2 and symbol_string(key) == "LEFT":
-			self.spaceship.do(move_left)
-		if self.spaceship.x<=375-20-self.spaceship.width/2 and symbol_string(key) == "RIGHT":
-			self.spaceship.do(Reverse(move_left))
+            break
 
-	def update(self,dt):
-		data = self.stream.read(self.CHUNK,exception_on_overflow = False)
-		sample = np.fromstring(data, dtype=aubio.float_type)
-		pitch=self.pDetection(sample)[0]
-		volume=np.sum(sample**2)/len(sample)
-		if(volume > 0.0002):
-			new_bullet=Bullet(self.spaceship.x, self.spaceship.y)
-			self.bullets.add(new_bullet)
+        if lives > 0:
+            pressed_keys = pygame.key.get_pressed()
+            if pressed_keys[pygame.K_UP]:
+                ship.y = ship.y - 10
+            if pressed_keys[pygame.K_DOWN]:
+                ship.y = ship.y + 10
+            if pressed_keys[pygame.K_LEFT]:
+                ship.x = ship.x - 10
+            if pressed_keys[pygame.K_RIGHT]:
+                ship.x = ship.x + 10
 
-def main():
-	#scroller for testing background
-	director.init(width=WIDTH, height=HEIGHT, autoscale=False, resizable=False)
-	scroller = ScrollingManager()
-	mapLayer = load("assets2/map/background.tmx")["Tile Layer 1"]
-	scroller.add(mapLayer)
-	main_scene = cocos.scene.Scene(scroller, Spaceship())
-	director.run(main_scene)
+            # Stop ship going out of bounds
+            if ship.y < 10:
+                ship.y = 10
 
-if __name__=="__main__":
-	main()
+            if ship.y > window.get_height() - 10:
+                ship.y = window.get_height() - 10
+
+            if ship.x < 0:
+                ship.x = 0
+
+            if ship.x > window.get_width() - ship_image.get_width():
+                ship.x = window.get_width() - ship_image.get_width()
+
+    data = stream.read(CHUNK,exception_on_overflow = False)
+    sample = np.fromstring(data, dtype=aubio.float_type)
+    pitch=voiceDetection(sample)[0]
+    volume=np.sum(sample**2)/len(sample)
+    #print("Volume: %s" % (volume))
+    #print("Pitch: %s" % (pitch))
+    if (volume > 0.001) and (lives > 0):
+        fire_bullet()
+
+    for bullet in bullets:
+        bullet.y = bullet.y - 13
+
+    bullets = [bullet for bullet in bullets if bullet.y > - bullet_image.get_height() and not bullet.used]
+
+    frames_until_next_alien = frames_until_next_alien - 1
+    if frames_until_next_alien <= 0:
+        frames_until_next_alien = random.randrange(30, 100)
+        add_alien()
+
+    ship.red = max(0, ship.red - 10)
+    ship.alpha = max(0, ship.alpha - 2)
+    ship_rect = get_sprite_rectangle(ship)
+
+    for alien in aliens:
+        alien.y = alien.y + 3
+        if alien.hit:
+            alien.alpha = max(0, alien.alpha - 10)
+
+    aliens = [alien for alien in aliens if alien.y < window.get_height() and not (alien.hit and alien.alpha == 0)]
+
+    frames_until_next_star = frames_until_next_star - 1
+    if frames_until_next_star <= 0:
+        frames_until_next_star = random.randrange(10, 30)
+        add_star()
+
+    for star in stars:
+        star.y = star.y + 2
+
+    stars = [star for star in stars if star.y < window.get_height()]
+
+    for alien in aliens:
+        if alien.hit:
+            continue
+        alien_rect = get_sprite_rectangle(alien)
+        if alien_rect.colliderect(ship_rect) and lives > 0:
+            alien.hit = True
+            # alien.x = alien.x - 6
+            # alien.y = alien.y - 6
+            lives = lives - 1
+            if lives == 0:
+                # ship.x = ship.x - 50
+                ship.alpha = 255
+            else:
+                ship.red = 255
+            continue
+        for bullet in bullets:
+            if alien_rect.colliderect(get_sprite_rectangle(bullet)):
+                alien.hit = True
+                # alien.x = alien.x - 6
+                # alien.y = alien.y - 6
+                bullet.used = True
+                score = score + 10
+                highest_score = max(score, highest_score)
+                continue
+
+    window.fill(background)
+    if lives == 0:
+        tmp = pygame.Surface(ship_image.get_size(), pygame.SRCALPHA, 32)
+        tmp.fill( (255, 255, 255, ship.alpha) )
+        tmp.blit(ship_image, (0,0), ship_image.get_rect(), pygame.BLEND_RGBA_MULT)
+        ship.image = tmp
+        # tmp = pygame.Surface(ship_image_destroyed.get_size(), pygame.SRCALPHA, 32)
+        # tmp.fill( (255, 255, 255, ship.alpha) )
+        # tmp.blit(ship_image_destroyed, (0,0), ship_image_destroyed.get_rect(), pygame.BLEND_RGBA_MULT)
+        # ship.image = tmp
+    if ship.red > 0:
+        tmp = pygame.Surface(ship_image.get_size(), pygame.SRCALPHA, 32)
+        tmp.fill( (255, 255 - ship.red, 255 - ship.red, 255) )
+        tmp.blit(ship_image, (0,0), ship_image.get_rect(), pygame.BLEND_RGBA_MULT)
+        ship.image = tmp
+
+    for star in stars:
+        display_sprite(star)
+    display_sprite(ship)
+    for bullet in bullets:
+        display_sprite(bullet)
+    for alien in aliens:
+        if alien.hit:
+            tmp = pygame.Surface(alien_image.get_size(), pygame.SRCALPHA, 32)
+            tmp.fill( (255, 255, 255, alien.alpha) )
+            tmp.blit(alien_image, (0,0), alien_image.get_rect(), pygame.BLEND_RGBA_MULT)
+            alien.image = tmp
+            # tmp = pygame.Surface(alien_dead_image.get_size(), pygame.SRCALPHA, 32)
+            # tmp.fill( (255, 255, 255, alien.alpha) )
+            # tmp.blit(alien_dead_image, (0,0), alien_dead_image.get_rect(), pygame.BLEND_RGBA_MULT)
+            # alien.image = tmp
+        display_sprite(alien)
+
+    score_text = font.render("SCORE: " + str(score), 1, foreground)
+    score_text_pos = score_text.get_rect()
+    score_text_pos.right = window.get_width() - 10
+    score_text_pos.top = 10
+    window.blit(score_text, score_text_pos)
+    highest_score_text = font.render("HIGHEST SCORE: " + str(highest_score), 1, foreground)
+    highest_score_text_pos = highest_score_text.get_rect()
+    highest_score_text_pos.left = (window.get_width() - (highest_score_text_pos.right-highest_score_text_pos.left))/2
+    highest_score_text_pos.top = 10
+    window.blit(highest_score_text, highest_score_text_pos)
+    lives_text = font.render("LIVES: " + str(lives), 1, foreground)
+    window.blit(lives_text, (10, 10))
+    if (lives == 0) and (ship.alpha == 0):
+        restart_text = font.render("PRESS SPACE TO RESTART THE GAME", 1, foreground)
+        restart_text_pos = restart_text.get_rect()
+        restart_text_pos.left = (window.get_width() - (restart_text_pos.right-restart_text_pos.left))/2
+        restart_text_pos.bottom = window.get_height() - 10
+        window.blit(restart_text, restart_text_pos)
+
+    pygame.display.flip()
+    clock.tick(50)
